f9539df6b915487c61ed87a0964113ab452a8063
jeangrmontreal <jeangr17@gmail.com>
2026-03-28 11:17:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 import streamlit as st
 from supabase import create_client
 
-st.title("🚀 APOLLO v2")
+st.title("🚀 APOLLO v3")
 
 SUPABASE_URL = "https://obhfwfkfeyfoiyuwczbe.supabase.co"
-SUPABASE_KEY = "sb_publishable__6hcsOxp7_6blIRz-nOphQ_8RZCKW2d"
+SUPABASE_KEY = ""
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 st.success("✅ Conectado a Supabase")
 
-# 🔥 PROBAR LECTURA
-try:
-    res = supabase.table("usuarios").select("*").limit(5).execute()
-    st.success("✅ Tabla accesible")
-    st.write(res.data)
-except Exception as e:
-    st.error("❌ Error leyendo tabla")
+# 🔥 LOGIN SIMPLE
+nombre = st.text_input("👤 Tu nombre")
+
+if st.button("Entrar"):
+
+    # buscar usuario
+    res = supabase.table("usuarios").select("*").eq("nombre", nombre).execute()
+
+    if res.data:
+        st.success(f"Bienvenido de nuevo {nombre} 🔥")
+        user = res.data[0]
+    else:
+        # crear usuario
+        supabase.table("usuarios").insert({
+            "nombre": nombre,
+            "historial": [],
+            "pesos": []
+        }).execute()
+
+        st.success(f"Usuario {nombre} creado 🚀")
+
+        res = supabase.table("usuarios").select("*").eq("nombre", nombre).execute()
+        user = res.data[0]
+
+    st.write(user)
