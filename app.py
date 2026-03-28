d300b23467294bd3a906d76682d2cb6326102d91
jeangrmontreal <jeangr17@gmail.com>
2026-03-28 11:14:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import streamlit as st
 from supabase import create_client
 
-st.title("🚀 ROCIO TE AMO")
+st.title("🚀 APOLLO v2")
 
 SUPABASE_URL = "https://obhfwfkfeyfoiyuwczbe.supabase.co"
 SUPABASE_KEY = "sb_publishable__6hcsOxp7_6blIRz-nOphQ_8RZCKW2d"
 
-# conectar
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+st.success("✅ Conectado a Supabase")
+
+# 🔥 PROBAR LECTURA
 try:
-    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-    st.success("✅ Conectado a Supabase")
-except:
-    st.error("❌ Error conexión")
+    res = supabase.table("usuarios").select("*").limit(5).execute()
+    st.success("✅ Tabla accesible")
+    st.write(res.data)
+except Exception as e:
+    st.error("❌ Error leyendo tabla")
